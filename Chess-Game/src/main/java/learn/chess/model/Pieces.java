0ee912c775124c004fa1319565998971f6d1eb6c
Chris <miller.christopher618@gmail.com>
2021-11-01 21:38:45 -0500

    Committing Starting Board. Will update Board Class Later

--- Chess-Game/src/main/java/learn/chess/model/Pieces.java
+++ Chess-Game/src/main/java/learn/chess/model/Pieces.java
@@ -1,10 +1,39 @@
 package learn.chess.model;
 
 public enum Pieces {
-    PAWN,
-    BISHOP,
-    ROOK,
-    KNIGHT,
-    QUEEN,
-    KING,
+    WHITE_PAWN(" P ", Color.WHITE),
+    WHITE_BISHOP(" B ",Color.WHITE),
+    WHITE_ROOK(" R ", Color.WHITE),
+    WHITE_KNIGHT(" N ", Color.WHITE),
+    WHITE_QUEEN(" Q ", Color.WHITE),
+    WHITE_KING(" K ", Color.WHITE),
+    BLACK_PAWN(" p ", Color.BLACK),
+    BLACK_BISHOP(" b ", Color.BLACK),
+    BLACK_ROOK(" r ", Color.BLACK),
+    BLACK_KNIGHT(" n ", Color.BLACK),
+    BLACK_QUEEN(" q ", Color.BLACK),
+    BLACK_KING(" k ", Color.BLACK),
+    EMPTY("   ", Color.CLEAR);
+
+    private String notation;
+    private Color color;
+
+    public String getNotation() {
+        return notation;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    Pieces(String notation, Color color) {
+        this.notation = notation;
+        this.color = color;
+    }
+
+    public enum Color{
+        WHITE,
+        BLACK,
+        CLEAR
+    }
 }
